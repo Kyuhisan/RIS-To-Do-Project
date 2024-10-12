f6cc637aa8d66dc80e51f3239f3261478a49792c
Matic Kuhar <matic.kuhar1@student.um.si>
2024-10-12 12:37:45 +0200

    initial commit

---
+++ Backend/src/test/java/si/um/feri/Backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package si.um.feri.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
